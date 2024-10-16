fb37085116ce59739b4182ba85e6892669d137f7
Joe Marini <cjmarini68@gmail.com>
2024-10-16 21:31:04 +0000

    comprehensions start state

--- Start/4. Sequences/sequence_slicing.py
+++ Start/4. Sequences/sequence_slicing.py
@@ -6,30 +6,22 @@ from collections import deque
 names = ["Jim", "Pam", "Creed", "Michael", "Dwight", "Oscar", "Kevin", "Phyllis"]
 
 # a slice is a subset of a sequence. The form is [start:stop:step]
-print(names[1:4])
+
 
 # using a step 
-print(names[0:7:2])
+
 
 # shorthand
-print(names[:3])
-print(names[5:])
+
 
 # reversing with step of -1
-print(names[::-1])
 
-newnames = ["Andy", "Stanley", "Angela"]
-names[2:4] = newnames
-print(names)
+
+# assigning sequences
+
 
 # the del operator works with slices
-del names[0:2]
-print(names)
+
 
 # not all sequence types support slicing, however
 deque_names = deque(["Jim", "Pam", "Creed", "Michael", "Dwight", "Oscar", "Kevin", "Phyllis"])
-for name in deque_names:
-    print(name, " ", end="")
-print()
-print(len(deque_names))
-print(deque_names[1:4]) # TypeError!
