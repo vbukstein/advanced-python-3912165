774d78ef0a108185a53c4cac3c8840bb50fe4d6d
Joe Marini <cjmarini68@gmail.com>
2024-10-16 18:08:34 +0000

    starting state

--- Start/2. Iterators/for_else.py
+++ Start/2. Iterators/for_else.py
@@ -17,14 +17,3 @@ print(findname("Creed"))
 print(findname("Tom"))
 
 # Check if a number is prime
-
-def is_prime(num):
-    for i in range(2, num):
-        if num % i == 0:
-            print(num, "is not a prime number")
-            break
-    else:
-        print(num, "is a prime number")
-
-is_prime(31)
-is_prime(56)
