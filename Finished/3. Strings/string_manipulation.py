47c66e7c64b67214303eb467aca3113d15088b86
joemarini <cjmarini68@gmail.com>
2024-10-16 22:30:41 -0700

    start state

--- Finished/3. Strings/string_manipulation.py
+++ Finished/3. Strings/string_manipulation.py
@@ -1,1 +1,26 @@
 # Example file for Advanced Python by Joe Marini
+# Manipulating string content
+
+
+test_str = "The quick, brown fox jumps over the lazy dog."
+
+# upper, lower, title
+print(test_str.upper())
+print(test_str.lower())
+print(test_str.title())
+
+# strip, lstrip, rstrip
+test_str2 = "   This string has whitespace   "
+print(test_str2.strip())
+print(test_str2.lstrip())
+print(test_str2.rstrip())
+
+# split creates a sequence from a single string
+words = test_str.split()
+print(words)
+
+# join concatenates an iterable into a single string
+words = ["Hello", "world", "from", "Python"]
+separator = " "
+sentence = separator.join(words)
+print(sentence)
--- Start/3. Strings/string_manipulation.py
+++ Start/3. Strings/string_manipulation.py
@@ -1,1 +1,16 @@
 # Example file for Advanced Python by Joe Marini
+# Manipulating string content
+
+
+test_str = "The quick, brown fox jumps over the lazy dog."
+
+# upper, lower, title
+
+
+# strip, lstrip, rstrip
+
+
+# split creates a sequence from a single string
+
+
+# join concatenates an iterable into a single string
