a4780179f3b5b23a0b2e2188515b7a1185325bcd
Joe Marini <cjmarini68@gmail.com>
2024-10-17 22:29:31 +0000

    start state

--- Start/3. Strings/string_search.py
+++ Start/3. Strings/string_search.py
@@ -3,23 +3,23 @@
 
 sample_text = "The quick brown fox jumps over the lazy dog."
 
-# TODO: Using find() to find the first occurrence of a substring
+# Using find() to find the first occurrence of a substring
 
 
-# TODO: Example with optional start and end parameters
+# Example with optional start and end parameters
 
 
-# TODO: Using index() to find the first occurrence of a substring (raises ValueError if not found)
+# Using index() to find the first occurrence of a substring (raises ValueError if not found)
 
 
-# TODO: The 'in' operator can be used for Boolean testing:
+# The 'in' operator can be used for Boolean testing:
 
 
-# TODO: Using rfind() to find the last occurrence of a substring
+# Using rfind() to find the last occurrence of a substring
 
 
-# TODO: Using rindex() to find the last occurrence of a substring (raises ValueError if not found)
+# Using rindex() to find the last occurrence of a substring (raises ValueError if not found)
 
 
-# TODO: The replace() function will find content in the string and replace it
+# The replace() function will find content in the string and replace it
 
