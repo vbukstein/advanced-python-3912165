f8696cf26c52e1e507230d273b005fba112031f3
Vadim Bukstein <vbukstein@gmail.com>
2025-08-03 11:32:26 +0300

    Train pyscope

--- Start/1. PythonFeatures/pyscope.py
+++ Start/1. PythonFeatures/pyscope.py
@@ -3,12 +3,20 @@
 
 
 # declare a variable within the global scope
+x=1
 
 
 # define a local function with a variable "x"
-
+def test():
+  x=10
+  print(x)
 
 # Run the test function and observe the two results
+test()
+print(x)
 
+x=x+5
+print(x)
+test()
 
 # Nested functions create inner scopes. These are called closures:
